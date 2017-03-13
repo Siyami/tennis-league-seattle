b4e2fac9b61911fac4812b70db7cd390cafbc95f
Siyami Avci <siyamiavci@gmail.com>
2017-03-13 11:46:22 -0700

    Close #6 Add player registration

--- routes/players.js
+++ routes/players.js
@@ -1,61 +1,15 @@
 'use strict';
 
 const bcrypt = require('bcrypt-as-promised');
-const boom = require('boom');
 const express = require('express');
-const jwt = require('jsonwebtoken');
-const knex = require('../knex');
-const { camelizeKeys, decamelizeKeys } = require('humps');
 
-// eslint-disable-next-line new-cap
 const router = express.Router();
 
 router.post('/players', (req, res, next) => {
-  const { email, password } = req.body;
-
-  if (!email || !email.trim()) {
-    return next(boom.create(400, 'Email must not be blank'));
-  }
-
-  if (!password || password.length < 8) {
-    return next(boom.create(
-      400,
-      'Password must be at least 8 characters long'
-    ));
-  }
-
-  knex('players')
-    .where('email', email)
-    .first()
-    .then((user) => {
-      if (user) {
-        throw boom.create(400, 'Email already exists');
-      }
-
-      return bcrypt.hash(password, 12);
-    })
-    .then((hashedPassword) => {
-      const { firstName, lastName } = req.body;
-      const insertUser = { firstName, lastName, email, hashedPassword };
-
-      return knex('players').insert(decamelizeKeys(insertUser), '*');
-    })
-    .then((rows) => {
-      const user = camelizeKeys(rows[0]);
-      const claim = { userId: user.id };
-      const token = jwt.sign(claim, process.env.JWT_KEY, {
-        expiresIn: '7 days'
-      });
-
-      res.cookie('token', token, {
-        httpOnly: true,
-        expires: new Date(Date.now() + 1000 * 60 * 60 * 24 * 7),  // 7 days
-        secure: router.get('env') === 'production'
-      });
-
-      delete user.hashedPassword;
-
-      res.send(user);
+  bcrypt.hash(req.body.password, 12)
+    .then((hashed_password) => {
+      console.log(req.body.email, hashed_password);
+      res.sendStatus(200);
     })
     .catch((err) => {
       next(err);
