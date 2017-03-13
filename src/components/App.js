55c599c13555edf1e2f48d807fcbe96026d04233
Siyami Avci <siyamiavci@gmail.com>
2017-03-13 01:26:11 -0700

    Close #3 Add Courts and Map components

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,68 @@
 import React, { Component } from 'react';
 // import logo from './logo.svg';
 import '../App.css';
+import Map from './Map'
+import Courts from './Courts'
+import superagent from 'superagent'
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        Hello
-      </div>
-    );
-  }
+	constructor(){
+		super()
+		this.state = {
+			courts: []
+		}
+	}
+
+	componentDidMount(){
+		const url = 'https://api.foursquare.com/v2/venues/search?v=20140806&ll=47.6062,-122.24&client_id=XMWHRJ1GC53RBBVTDKPVN0FE4ZFQ5BT2DEF3TQYWPZYFWKBT&client_secret=1JHMHNHRO25DFMFXWAWBAKQQQXK3ARSE4ZWRA5LI1IZ1WLS4&query=tennis+courts'
+
+		superagent
+		.get(url)
+		.query(null)
+		.set('Accept', 'text/json')
+		.end((error, response) => {
+
+			const courts = response.body.response.venues
+
+			this.setState({
+				courts: courts
+			})
+		})
+
+	}
+
+	render(){
+		const location = {
+			lat: 47.6062,
+			lng: -122.24
+		}
+
+		// you can manually include markers
+    // const markers = [
+    //   {
+    //     location: {
+    //       lat: 47.6062,
+    // 			lng: -122.24
+    //     }
+    //   },
+    //   {
+    //     location: {
+    //       lat: 47.6562,
+    // 			lng: -122.34
+    //     }
+    //   }
+    // ]
+
+		return (
+			<div>
+				<div style={{width:600, height:600}}>
+					<Map center={location} markers={this.state.courts} />
+				</div>
+
+				<Courts courts={this.state.courts} />
+			</div>
+		)
+	}
 }
 
 export default App;
---
+++ src/components/Courts.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+
+class Courts extends Component {
+	render(){
+		const list = this.props.courts.map((court, i) => {
+			return (
+				<li key={i}>{court.name}</li>
+			)
+		})
+
+		return (
+			<div>
+				Tennis Courts
+				<ol>
+					{list}
+				</ol>
+
+			</div>
+		)
+	}
+}
+
+export default Courts
