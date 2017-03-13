e68cd20f29ae15bc9697c52c392654417c4b41a0
Siyami Avci <siyamiavci@gmail.com>
2017-03-13 10:07:33 -0700

    Close #2 add a server

---
+++ app.js
@@ -0,0 +1,61 @@
+'use strict';
+
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
+
+const boom = require('boom');
+const express = require('express');
+const morgan = require('morgan');
+const path = require('path');
+const app = express();
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const jwt = require('jsonwebtoken');
+
+app.use(morgan('short'));
+app.use(bodyParser.json());
+app.disable('x-powered-by');
+app.use(cookieParser());
+
+const authorize = function(req, res, next) {
+ jwt.verify(req.cookies.token, process.env.JWT_KEY, (err, payload) => {
+   if (err) {
+     return next(boom.create(401, 'Unauthorized'));
+   }
+
+   req.claim = payload;
+
+   next();
+ });
+};
+
+// app.use('/api', require('./routes/players'));
+// app.use('/api', require('./routes/token'));
+// app.use('/api', authorize, require('./routes/scores'));
+// app.use('/api', authorize, require('./routes/leagues'));
+// app.use('/api', authorize, require('./routes/courts'));
+
+app.use(express.static(path.resolve(__dirname, '..', 'build')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '..', 'build', 'index.html'));
+});
+
+app.use((_req, res) => {
+  res.sendStatus(404);
+});
+
+app.use((err, _req, res, _next) => {
+  if (err.output && err.output.statusCode) {
+     return res
+     .status(err.output.statusCode)
+       .set('Content-Type', 'text/plain')
+       .send(err.output.payload.message)
+   }
+
+  console.error(err.stack);
+  res.sendStatus(500);
+});
+
+module.exports = app;
