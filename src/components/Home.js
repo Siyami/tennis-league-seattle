557f5b4d5e915b54b2664a98055613896e9aa918
Siyami Avci <siyamiavci@gmail.com>
2017-03-21 13:24:43 -0700

    Close #34 Add home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,34 @@
 import React, { Component } from 'react';
+import { Grid, Carousel } from 'react-bootstrap';
 
 class Home extends Component {
   render() {
     return (
-      <div>
-        Hello from Home Component
-      </div>
+      <Grid>
+        <Carousel>
+          <Carousel.Item>
+            <img width={900} height={500} alt="900x500" src=""/>
+            <Carousel.Caption>
+              <h3>First slide label</h3>
+              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <img width={900} height={500} alt="900x500" src=""/>
+            <Carousel.Caption>
+              <h3>Second slide label</h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <img width={900} height={500} alt="900x500" src=""/>
+            <Carousel.Caption>
+              <h3>Third slide label</h3>
+              <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+        </Carousel>
+      </Grid>
     )
   }
 }
